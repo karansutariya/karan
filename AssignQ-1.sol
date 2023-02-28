25d7463cab70aaed26e484a2b4cc21e55babed69
Karan Sutariya <karansutariya30@gmail.com>
2023-02-28 12:58:47 +0530

    initial commit

---
+++ AssignQ-1.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.0;
+
+contract AssignQ1 {
+
+    function printHelloWorld() public view returns (string memory) {
+
+        return "Hello World";
+    }
+}
---
+++ AssignQ-2.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.8.0;
+
+contract counter {
+
+    function add (uint a, uint b) public view returns (uint) 
+        returns a+b;
+    }
+}
